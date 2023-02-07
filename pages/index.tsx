6324544ec811946c2494dd82d8577bc3501e7a0b
Samuel Pokam <dixiades@gmail.com>
2023-02-07 13:37:30 +0100

    updates

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,18 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
+import React, { useState } from "react";
 
 const Home: NextPage = () => {
+  const [active, setActive] = useState(false);
+  const handleClick = () => {
+    setActive(true);
+    setTimeout(() => {
+      setActive(false);
+    }, 200);
+    new Audio("/Heater-1.mp3").play();
+  };
+
   return (
     <div className=" bg-slate-300 h-screen ">
       <Head>
@@ -13,31 +23,35 @@ const Home: NextPage = () => {
         <h1 className=" text-6xl">Drum Machine</h1>
         <h3 className=" text-2xl py-3">Built with love</h3>
       </div>
-      <div className="">
+      <div className="flex h-3/5">
         <div
-          className=" bg-lime-700 w-10/12 lg:w-1/2 m-auto rounded-lg border-black border-2 p-12 "
+          className=" bg-lime-700 w-10/12 md:w-1/2 m-auto rounded-lg border-black border-2 p-12 "
           id="drum-machine"
         >
           <div id="display"></div>
           <div className="flex gap-2 pb-2">
-            <div
-              className="drum-pad text-3xl font-bold w-24 h-24 flex justify-center items-center rounded-md shadow-xl opacity-80 hover:opacity-100 bg-red-400"
+            <button
+              className={`drum-pad text-3xl font-bold w-24 h-24 flex justify-center items-center rounded-md shadow-xl cursor-pointer opacity-80 hover:opacity-100 ${
+                active ? "bg-emerald-400" : "bg-red-400"
+              }`}
               id="Heater 1"
+              onClick={handleClick}
+              onKeyDown={handleClick}
             >
               Q
-            </div>
-            <div
+            </button>
+            <button
               className="drum-pad text-3xl font-bold w-24 h-24 flex justify-center items-center rounded-md shadow-xl opacity-80 hover:opacity-100 bg-red-400"
               id="Heater 2"
             >
               W
-            </div>
-            <div
+            </button>
+            <button
               className="drum-pad text-3xl font-bold w-24 h-24 flex justify-center items-center rounded-md shadow-xl opacity-80 hover:opacity-100 bg-red-400"
               id="Heater 3"
             >
               E
-            </div>
+            </button>
           </div>
           <div className="flex gap-2 pb-2">
             <div
