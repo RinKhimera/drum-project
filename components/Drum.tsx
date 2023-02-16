dd5160801402f3ccb2cf4f0583410d5e857d31cf
Samuel Pokam <dixiades@gmail.com>
2023-02-16 16:41:55 +0100

    Update Drum.tsx

--- components/Drum.tsx
+++ components/Drum.tsx
@@ -1,18 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "./Button";
 
 const Drum = () => {
   const buttons = [
-    { id: "Heater-1", audioPath: "/Heater-1.mp3", keyTrigger: "Q" },
-    { id: "Heater-2", audioPath: "/Heater-2.mp3", keyTrigger: "W" },
-    { id: "Heater-3", audioPath: "/Heater-3.mp3", keyTrigger: "E" },
-    { id: "Heater-4", audioPath: "/Heater-4.mp3", keyTrigger: "A" },
-    { id: "Clap", audioPath: "/Clap.mp3", keyTrigger: "S" },
-    { id: "Open-HH", audioPath: "/Open-HH.mp3", keyTrigger: "D" },
-    { id: "Kick-n'-Hat", audioPath: "/Kick-n-Hat.mp3", keyTrigger: "Z" },
-    { id: "Kick", audioPath: "/Kick.mp3", keyTrigger: "X" },
-    { id: "Closed-HH", audioPath: "/Closed-HH.mp3", keyTrigger: "C" },
+    {
+      id: "Heater-1",
+      audioPath: "/Heater-1.mp3",
+      keyTrigger: "Q",
+      name: "Heater 1",
+    },
+    {
+      id: "Heater-2",
+      audioPath: "/Heater-2.mp3",
+      keyTrigger: "W",
+      name: "Heater 2",
+    },
+    {
+      id: "Heater-3",
+      audioPath: "/Heater-3.mp3",
+      keyTrigger: "E",
+      name: "Heater 3",
+    },
+    {
+      id: "Heater-4",
+      audioPath: "/Heater-4.mp3",
+      keyTrigger: "A",
+      name: "Heater 4",
+    },
+    { id: "Clap", audioPath: "/Clap.mp3", keyTrigger: "S", name: "Clap" },
+    {
+      id: "Open-HH",
+      audioPath: "/Open-HH.mp3",
+      keyTrigger: "D",
+      name: "Open HH",
+    },
+    {
+      id: "Kick-n'-Hat",
+      audioPath: "/Kick-n-Hat.mp3",
+      keyTrigger: "Z",
+      name: "Kick n' Hat",
+    },
+    { id: "Kick", audioPath: "/Kick.mp3", keyTrigger: "X", name: "Kick" },
+    {
+      id: "Closed-HH",
+      audioPath: "/Closed-HH.mp3",
+      keyTrigger: "C",
+      name: "Closed HH",
+    },
   ];
+
+  const [currentAudioName, setCurrentAudioName] = useState("");
+
+  const handleButtonClick = (name) => {
+    setCurrentAudioName(name);
+  };
 
   return (
     <>
@@ -22,20 +63,27 @@ const Drum = () => {
       <div className="flex justify-center items-center h-screen">
         <div
           id="drum-machine"
-          className="grid bg-lime-700 w-4/5 lg:w-2/5 justify-items-center gap-5 m-auto rounded-lg border-black border-2 p-10 "
+          className="grid w-9/12 lg:w-2/3 2xl:w-1/2 bg-lime-700 justify-items-center gap-5 rounded-lg border-black border-2 p-5"
         >
           <div className="text-center">
             <h1 className=" text-6xl">Drum Machine</h1>
           </div>
           <div id="buttons" className="">
-            <div className="flex gap-2 pb-2">
-              {buttons.map(({ id, audioPath, keyTrigger }) => (
-                <Button id={id} audioPath={audioPath} keyTrigger={keyTrigger} />
+            <div className="flex flex-wrap justify-center gap-2">
+              {buttons.map(({ id, audioPath, keyTrigger, name }) => (
+                <Button
+                  id={id}
+                  audioPath={audioPath}
+                  keyTrigger={keyTrigger}
+                  onClick={() => handleButtonClick(name)}
+                />
               ))}
             </div>
           </div>
           <div id="display">
-            <div className="bg-amber-400 p-3 text-3xl text-center w-60"></div>
+            <div className="bg-amber-400 p-3 text-3xl text-center w-60">
+              {currentAudioName}
+            </div>
             <h1 className="pt-5">Copyright (c) 2023 Samuel Pokam</h1>
           </div>
         </div>
