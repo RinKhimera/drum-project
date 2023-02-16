1c7c87a572d1ac0def3943f52b8227bddd650185
Samuel Pokam <dixiades@gmail.com>
2023-02-16 20:10:23 +0100

    Update Drum.tsx

--- components/Drum.tsx
+++ components/Drum.tsx
@@ -5,45 +5,50 @@ const Drum = () => {
   const buttons = [
     {
       id: "Heater-1",
-      audioPath: "/Heater-1.mp3",
+      audioPath: "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3",
       keyTrigger: "Q",
       name: "Heater 1",
     },
     {
       id: "Heater-2",
-      audioPath: "/Heater-2.mp3",
+      audioPath: "https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3",
       keyTrigger: "W",
       name: "Heater 2",
     },
     {
       id: "Heater-3",
-      audioPath: "/Heater-3.mp3",
+      audioPath: "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3",
       keyTrigger: "E",
       name: "Heater 3",
     },
     {
       id: "Heater-4",
-      audioPath: "/Heater-4.mp3",
+      audioPath: "https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3",
       keyTrigger: "A",
       name: "Heater 4",
     },
     { id: "Clap", audioPath: "/Clap.mp3", keyTrigger: "S", name: "Clap" },
     {
       id: "Open-HH",
-      audioPath: "/Open-HH.mp3",
+      audioPath: "https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3",
       keyTrigger: "D",
       name: "Open HH",
     },
     {
       id: "Kick-n'-Hat",
-      audioPath: "/Kick-n-Hat.mp3",
+      audioPath: "https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3",
       keyTrigger: "Z",
       name: "Kick n' Hat",
     },
-    { id: "Kick", audioPath: "/Kick.mp3", keyTrigger: "X", name: "Kick" },
+    {
+      id: "Kick",
+      audioPath: "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3",
+      keyTrigger: "X",
+      name: "Kick",
+    },
     {
       id: "Closed-HH",
-      audioPath: "/Closed-HH.mp3",
+      audioPath: "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3",
       keyTrigger: "C",
       name: "Closed HH",
     },
@@ -62,40 +67,35 @@ const Drum = () => {
   };
 
   return (
-    <>
-      <title>Drum Machine</title>
-      <link rel="icon" href="/favicon.ico" />
-
-      <div className="flex justify-center items-center h-screen">
-        <div
-          id="drum-machine"
-          className="grid w-9/12 lg:w-2/3 2xl:w-1/2 bg-gradient-to-r from-cyan-500 to-blue-500 justify-items-center gap-5 rounded-lg border-black border-2 p-5"
-        >
-          <div className="text-center">
-            <h1 className=" text-6xl">Drum Machine</h1>
-          </div>
-          <div id="buttons" className="">
-            <div className="flex flex-wrap justify-center gap-2">
-              {buttons.map(({ id, audioPath, keyTrigger, name }) => (
-                <Button
-                  id={id}
-                  audioPath={audioPath}
-                  keyTrigger={keyTrigger}
-                  name={name}
-                  onClick={() => {
-                    handleButtonClick(name);
-                    handleAudioPlay(name);
-                  }}
-                />
-              ))}
-            </div>
-          </div>
-          <div className="justify-self-end text-lg">
-            Copyright (c) 2023 Samuel Pokam
+    <div className="flex justify-center items-center h-screen">
+      <div
+        id="drum-machine"
+        className="grid w-9/12 lg:w-2/3 2xl:w-1/2 bg-gradient-to-r from-cyan-500 to-blue-500 justify-items-center gap-5 rounded-lg border-black border-2 p-5"
+      >
+        <div className="text-center">
+          <h1 className=" text-6xl">Drum Machine</h1>
+        </div>
+        <div id="buttons" className="">
+          <div className="flex flex-wrap justify-center gap-2">
+            {buttons.map(({ id, audioPath, keyTrigger, name }) => (
+              <Button
+                id={id}
+                audioPath={audioPath}
+                keyTrigger={keyTrigger}
+                name={name}
+                onClick={() => {
+                  handleButtonClick(name);
+                  handleAudioPlay(name);
+                }}
+              />
+            ))}
           </div>
         </div>
+        <div className="justify-self-end text-lg">
+          Copyright (c) 2023 Samuel Pokam
+        </div>
       </div>
-    </>
+    </div>
   );
 };
 
